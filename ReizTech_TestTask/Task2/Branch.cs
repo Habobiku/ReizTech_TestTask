f35c1364d9598f8f508dce6bed51e8665e2b2924
Habobiku <gireygame@gmail.com>
2022-11-07 16:46:17 +0200

    #

---
+++ ReizTech_TestTask/Task2/Branch.cs
@@ -0,0 +1,51 @@
+﻿using System;
+namespace ReizTech_TestTask
+{
+    public class Branch
+    {
+      public IList<Branch> Branches { get;  private set; }
+      public string Name { get; private set; }
+        public Branch(String name)
+        {
+            Name = name;
+            Branches = new List<Branch>();
+        }
+
+        public Branch AddBranch(Branch branch)
+        {
+            Branches.Add(branch);
+            return branch;
+
+        }
+
+        public List<Branch> FindDepth()
+        {
+            if (Branches == null)
+                return null;
+            List<Branch> path = new List<Branch>();
+            foreach (Branch branch in Branches)
+            {
+                List<Branch> temp = branch.FindDepth();
+                     if (temp.Count > path.Count)
+                    path = temp;
+            }
+            
+            path.Insert(0, this);
+            return path;
+        }
+
+        public Branch FindBranch(string name)
+        {
+            foreach (Branch branch in Branches)
+            {
+                if (branch.Name != name)
+                    branch.FindBranch(name);
+                                                 
+                return branch;
+            }
+
+            return null;
+        }
+    }
+}
+
